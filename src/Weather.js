9aec0249c74f81e40ed253d78e1da8441904fa12
Tanaz2202 <tanaz.kh.ne@gmail.com>
2022-07-03 17:07:44 +0100

    Made an Axios call to test working

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,16 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
+  function displayWeather(response) {
+    console.log(response.data);
+  }
+
+  const apiKey = "31f529cdb0d991333ed2798a23f84dcc";
+  const unit = "metric";
+  let city = "New York";
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(displayWeather);
+
   let form = (
     <form>
       <input type="search" />
