1052d690b21af2dc528006ed08be35de8fddcff4
Tanaz2202 <tanaz.kh.ne@gmail.com>
2022-07-03 16:57:55 +0100

    Created Weather component and deleted unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./Weather";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Weather />
     </div>
   );
 }
