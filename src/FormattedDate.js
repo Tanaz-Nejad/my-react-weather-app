c4b74dd332468d2bea3e2d49c0601ed0d53c3712
Tanaz2202 <tanaz.kh.ne@gmail.com>
2022-07-03 18:35:30 +0100

    Added search city data via API and date componant

---
+++ src/FormattedDate.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function FormattedDate(props) {
+  console.log(props.date);
+  let dayIndex = props.date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  let hour = props.date.getHours();
+  if (hour < 10) {
+    hour = `0${hour}`;
+  }
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return (
+    <div>
+      {day} {hour}:{minutes}
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import FormattedDate from "./FormattedDate";
 
 export default function Weather() {
-  function handleSubmit() {
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
     const apiKey = "31f529cdb0d991333ed2798a23f84dcc";
     const unit = "metric";
-    const city = "Sidney";
+    const city = "sidney";
     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
     axios.get(apiUrl).then(displayWeather);
   }
 
   function displayWeather(response) {
-    console.log(response.data.main.temp);
+    setWeather({
+      temperature: Math.round(response.data.main.temp),
+      date: new Date(response.data.dt * 1000),
+      wind: Math.round(response.data.wind.speed),
+      humidity: Math.round(response.data.main.humidity),
+      description: response.data.weather[0].description,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
   }
 
   let form = (
     <form onSubmit={handleSubmit}>
-      <input type="search" />
+      <input type="search" onChange={updateCity} />
       <input type="submit" />
     </form>
   );
 
-  return <div className="Weather">{form}</div>;
+  return (
+    <div className="Weather">
+      {form}
+      <h1>{city}</h1>
+      <ul>
+        <li>{weather.temperature}</li>
+        <li>{weather.humidity}</li>
+        <li>{weather.wind}</li>
+        <li>{weather.description}</li>
+        <li>
+          <FormattedDate date={weather.date} />
+        </li>
+      </ul>
+    </div>
+  );
 }
