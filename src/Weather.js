9cfb281b349f5c179fcf68b2892be2bc57760ec8
Tanaz2202 <tanaz.kh.ne@gmail.com>
2022-07-03 17:47:50 +0100

    Axios call not working, testing again

--- src/Weather.js
+++ src/Weather.js
@@ -2,18 +2,20 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
-  function displayWeather(response) {
-    console.log(response.data);
+  function handleSubmit() {
+    const apiKey = "31f529cdb0d991333ed2798a23f84dcc";
+    const unit = "metric";
+    const city = "Sidney";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
+    axios.get(apiUrl).then(displayWeather);
   }
 
-  const apiKey = "31f529cdb0d991333ed2798a23f84dcc";
-  const unit = "metric";
-  let city = "New York";
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
-  axios.get(apiUrl).then(displayWeather);
+  function displayWeather(response) {
+    console.log(response.data.main.temp);
+  }
 
   let form = (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input type="search" />
       <input type="submit" />
     </form>
